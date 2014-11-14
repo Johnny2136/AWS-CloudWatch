751b78a1398ad1d94732423db0d8d2e133cae650
Johnny Johnson <johnny2136@gmail.com>
2014-11-14 12:43:40 -0500

    First draft of AWS Bash script
    Added to Git hub

---
+++ AWSCloudWatch.sh
@@ -0,0 +1,34 @@
+#!/bin/bash
+
+
+aws cloudwatch get-metric-statistics --metric-name CPUUtilization --start-time 11-01-14T23:18:00 --end-time 11-13-14T23:18:00 --period 3600 --namespace AWS/EC2 --statistics Maximum --dimensions Name=InstanceId,Value=i-1f3ef23a >APP_Server_CPUUtilization.txt
+
+aws cloudwatch get-metric-statistics --metric-name CPUUtilization --start-time 11-01-14T23:18:00 --end-time 11-13-14T23:18:00 --period 3600 --namespace AWS/EC2 --statistics Maximum --dimensions Name=InstanceId,Value=i-4c488769 >DB_Server_CPUUtilization.txt
+
+aws cloudwatch get-metric-statistics --metric-name CPUUtilization --start-time 11-01-14T23:18:00 --end-time 11-13-14T23:18:00 --period 3600 --namespace AWS/EC2 --statistics Maximum --dimensions Name=InstanceId,Value=i-5e3af87b >Web#1_Server_CPUUtilization.txt
+
+aws cloudwatch get-metric-statistics --metric-name CPUUtilization --start-time 11-01-14T23:18:00 --end-time 11-13-14T23:18:00 --period 3600 --namespace AWS/EC2 --statistics Maximum --dimensions Name=InstanceId,Value=i-6410df41 >Web#2_Server_CPUUtilization.txt
+
+aws cloudwatch get-metric-statistics --metric-name CPUUtilization --start-time 11-01-14T23:18:00 --end-time 11-13-14T23:18:00 --period 3600 --namespace AWS/EC2 --statistics Maximum --dimensions Name=InstanceId,Value=i-6510df40 >Web#3_Server_CPUUtilization.txt
+
+
+aws cloudwatch get-metric-statistics --metric-name NetworkBytesIn --start-time 11-01-14T23:18:00 --end-time 11-13-14T23:18:00 --period 3600 --namespace AWS/EC2 --statistics Maximum --dimensions Name=InstanceId,Value=i-1f3ef23a >APP_Server_NetworkBytesIn.txt
+
+aws cloudwatch get-metric-statistics --metric-name NetworkBytesIn --start-time 11-01-14T23:18:00 --end-time 11-13-14T23:18:00 --period 3600 --namespace AWS/EC2 --statistics Maximum --dimensions Name=InstanceId,Value=i-4c488769 >DB_Server_NetworkBytesIn.txt
+
+aws cloudwatch get-metric-statistics --metric-name NetworkBytesIn --start-time 11-01-14T23:18:00 --end-time 11-13-14T23:18:00 --period 3600 --namespace AWS/EC2 --statistics Maximum --dimensions Name=InstanceId,Value=i-5e3af87b >Web#1_Server_NetworkBytesIn.txt
+
+aws cloudwatch get-metric-statistics --metric-name NetworkBytesIn --start-time 11-01-14T23:18:00 --end-time 11-13-14T23:18:00 --period 3600 --namespace AWS/EC2 --statistics Maximum --dimensions Name=InstanceId,Value=i-6410df41 >Web#2_Server_NetworkBytesIn.txt
+
+aws cloudwatch get-metric-statistics --metric-name NetworkBytesIn --start-time 11-01-14T23:18:00 --end-time 11-13-14T23:18:00 --period 3600 --namespace AWS/EC2 --statistics Maximum --dimensions Name=InstanceId,Value=i-6510df40 >Web#3_Server_NetworkBytesIn.txt
+
+
+aws cloudwatch get-metric-statistics --metric-name NetworkBytesOut --start-time 11-01-14T23:18:00 --end-time 11-13-14T23:18:00 --period 3600 --namespace AWS/EC2 --statistics Maximum --dimensions Name=InstanceId,Value=i-1f3ef23a >APP_Server_NetworkBytesOut.txt
+
+aws cloudwatch get-metric-statistics --metric-name NetworkBytesOut --start-time 11-01-14T23:18:00 --end-time 11-13-14T23:18:00 --period 3600 --namespace AWS/EC2 --statistics Maximum --dimensions Name=InstanceId,Value=i-4c488769 >DB_Server_NetworkBytesOut.txt
+
+aws cloudwatch get-metric-statistics --metric-name NetworkBytesOut --start-time 11-01-14T23:18:00 --end-time 11-13-14T23:18:00 --period 3600 --namespace AWS/EC2 --statistics Maximum --dimensions Name=InstanceId,Value=i-5e3af87b >Web#1_Server_NetworkBytesOut.txt
+
+aws cloudwatch get-metric-statistics --metric-name NetworkBytesOut --start-time 11-01-14T23:18:00 --end-time 11-13-14T23:18:00 --period 3600 --namespace AWS/EC2 --statistics Maximum --dimensions Name=InstanceId,Value=i-6410df41 >Web#2_Server_NetworkBytesOut.txt
+
+aws cloudwatch get-metric-statistics --metric-name NetworkBytesOut --start-time 11-01-14T23:18:00 --end-time 11-13-14T23:18:00 --period 3600 --namespace AWS/EC2 --statistics Maximum --dimensions Name=InstanceId,Value=i-6510df40 >Web#3_Server_NetworkBytesOut.txt
